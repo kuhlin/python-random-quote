2d702dfc1c83665e2ffb75676f74f272d2364816
George Iulian Diaconu <kuhlin@gmail.com>
2020-06-25 16:12:39 +0200

    Some bisection search

--- the_basics/basics.py
+++ the_basics/basics.py
@@ -1,5 +1,16 @@
-import datetime
-x = datetime.datetime.now()
-print("The date and time is:", x)
-
-sudents_grades = [9.1, 8.8, 7.5]
+x = 25
+epsilon = 0.01
+numGuesses = 0
+low = 0.0
+high = max(1.0, x)
+ans = (high + low)/2.0
+while abs(ans**2 - x) >= epsilon:
+    print ('low =', low, 'high =', high, 'ans =', ans)
+    numGuesses += 1
+    if ans**2 < x:
+        low = ans
+    else:
+        high = ans
+    ans = (high + low)/2.0
+print ('numGuesses =', numGuesses)
+print (ans, 'is close to square root of', x)
